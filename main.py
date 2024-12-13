481f3865c4e519c9811766fd3b811600a9969e76
Hatsu_dev <mlopez1749@gmail.com>
2024-12-13 17:38:24 -0600

    added a character counting dictionary

--- main.py
+++ main.py
@@ -2,14 +2,28 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = get_word_count(text)
-    print(f"{word_count} words found in this document")
+    char_dict = get_char_dict(text)
+    print(char_dict)
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-    
+  
+
 def get_word_count(text):
     words = text.split()
     return len(words)
+
+
+def get_char_dict(text):
+    char = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in char:
+            char[lowered] += 1
+        else:
+            char[lowered] = 1
+    return char
 
 main()
