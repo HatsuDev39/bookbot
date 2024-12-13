b38051c2f70614208ac284dab8d163fb10e37810
Hatsu_dev <mlopez1749@gmail.com>
2024-12-13 15:24:56 -0600

    added word counter to main.py

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ def main():
         file_contents = f.read()
     print(file_contents)
 
-    
+    word_count = 0
+    words = file_contents.split()
+    for word in words:
+        word_count += 1
+    print(word_count)
 main()
