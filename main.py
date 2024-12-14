858df11a069f07249795c3262e2890012d1a5730
Hatsu_dev <mlopez1749@gmail.com>
2024-12-13 23:47:44 -0600

    added formatting and sorted character counter

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     char_dict = get_char_dict(text)
-    print(char_dict)
+    sorted_dict = sort_char_dict(char_dict)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in this document")
+    print()
 
+    for item in sorted_dict:
+        print(f"The {item["character"]} character was found {item["count"]} times")
+
+    print("--- End of report ---")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
   
-
 def get_word_count(text):
     words = text.split()
     return len(words)
-
 
 def get_char_dict(text):
     char = {}
@@ -25,5 +30,19 @@ def get_char_dict(text):
         else:
             char[lowered] = 1
     return char
+
+def sort_on(d):
+    return d["count"]
+
+def sort_char_dict(char_dict):
+    new_list = []
+    for char in char_dict:
+        sorted_dict = {}
+        if char.isalpha():
+            sorted_dict["character"] = char
+            sorted_dict["count"] = char_dict[char]
+            new_list.append(sorted_dict)
+    new_list.sort(reverse=True, key=sort_on)
+    return new_list
 
 main()
